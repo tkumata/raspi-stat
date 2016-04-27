1de507d13e5d04711862356d1ff4a93aedd0a501
tkumata <tkumata@gmail.com>
2016-04-27 13:43:24 +0900

    add raspi-stat.sh

---
+++ raspi-stat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+. /etc/os-release
+
+CPUINFO="===============\t=============================\n"
+VOLT=""
+RES=""
+
+CPUINFO="${CPUINFO}\nOS\t${PRETTY_NAME}"
+CPUINFO="${CPUINFO}\n===============\t=============================\n"
+CPUINFO="${CPUINFO}\nCPU Clock\t$(vcgencmd measure_clock arm|cut -d = -f 2)Hz\n"
+CPUINFO="${CPUINFO}\nCPU Temperature\t$(vcgencmd measure_temp|cut -d = -f 2)\n"
+CPUINFO="${CPUINFO}\n===============\t=============================\n"
+
+for id in core sdram_c sdram_i sdram_p; do
+    VOLT="${VOLT}\n$id\t$(vcgencmd measure_volts $id)\n"
+done
+
+RES=`cat << EoS
+${CPUINFO}
+${VOLT}
+EoS
+`
+
+RES="${RES}\n===============\t=============================\n"
+
+echo -e ${RES} | column -s $'\t' -t
