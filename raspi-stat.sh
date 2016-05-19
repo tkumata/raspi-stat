ca07203b04752c2a7b8fc570b670723a1a1ebd32
tkumata <tkumata@gmail.com>
2016-05-20 07:57:20 +0900

    fix

--- raspi-stat.sh
+++ raspi-stat.sh
@@ -23,18 +23,18 @@ while true; do
     clear
     CPUINFO=""
     VOLT=""
-    RES="=== OS =====\t====================\n"
+    RES="== OS ======\t==================\n"
 
     CPUINFO="${CPUINFO}\nName\t${NAME}"
     CPUINFO="${CPUINFO}\nVersion\t${VERSION}"
-    CPUINFO="${CPUINFO}\n=== CPU ====\t====================\n"
+    CPUINFO="${CPUINFO}\n== CPU =====\t==================\n"
     CPUINFO="${CPUINFO}\nClock arm\t$(vcgencmd measure_clock arm | cut -d = -f 2)Hz\n"
     CPUINFO="${CPUINFO}\nClock core\t$(vcgencmd measure_clock core | cut -d = -f 2)Hz\n"
     CPUINFO="${CPUINFO}\nTemperature\t$(vcgencmd measure_temp | cut -d = -f 2)\n"
-    CPUINFO="${CPUINFO}\n=== Mem ====\t====================\n"
+    CPUINFO="${CPUINFO}\n== Mem =====\t==================\n"
     CPUINFO="${CPUINFO}\nMem arm\t$(vcgencmd get_mem arm | cut -d = -f 2)\n"
     CPUINFO="${CPUINFO}\nMem gpu\t$(vcgencmd get_mem gpu | cut -d = -f 2)\n"
-    CPUINFO="${CPUINFO}\n=== Volt ===\t====================\n"
+    CPUINFO="${CPUINFO}\n== Volt ====\t==================\n"
 
     for id in core sdram_c sdram_i sdram_p; do
         VOLT="${VOLT}\n$id\t$(vcgencmd measure_volts $id)\n"
@@ -45,7 +45,7 @@ ${CPUINFO}
 ${VOLT}
 EoS
 `
-    RES="${RES}\n============\t====================\n"
+    RES="${RES}\n============\t==================\n"
 
     echo -e ${RES} | column -s $'\t' -t
 
