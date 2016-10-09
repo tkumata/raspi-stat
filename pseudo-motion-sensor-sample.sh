38fe0e7a9b7babfacf8cfcb7ae1b312d2c492e35
tkumata <tkumata@gmail.com>
2016-10-10 06:24:38 +0900

    fix

--- pseudo-motion-sensor-sample.sh
+++ pseudo-motion-sensor-sample.sh
@@ -7,7 +7,7 @@ set -eu
 THRESHOLD="45" # Your threshold of Wi-Fi RSSI.
 COUNT="0"
 COUNT_THRESHOLD="1"
-SLEEP_TIMER="0.5"
+SLEEP_TIMER="0.3"
 
 function led_fire_on() {
     if [ "$(pgrep -f 'led_fire')" = "" ]; then
@@ -23,14 +23,14 @@ function led_fire_off() {
 
 while :
 do
-    link_quality="$(cat /proc/net/wireless | tail -1 | awk '{print $3}' | sed -e 's/\.//')"
+    #link_quality="$(cat /proc/net/wireless | tail -1 | awk '{print $3}' | sed -e 's/\.//')"
     signal_level="$(cat /proc/net/wireless | tail -1 | awk '{print $4}' | sed -e 's/\-//' -e 's/\.//')"
 
     if [ "$THRESHOLD" -le "$signal_level" ]; then
         COUNT="$(($COUNT + 1))"
         #echo -e "$link_quality/70\t-$signal_level dBm"
         echo -n "x"
-        if [ "$COUNT" -gt "$COUNT_THRESHOLD" ]; then
+        if [ "$COUNT" -ge "$COUNT_THRESHOLD" ]; then
             led_fire_on
         fi
     else
