0fff440977ce6037737c08efc186416f4c829e6f
Tomokatsu Kumata <tkumata@gmail.com>
2018-08-08 12:35:14 +0900

    fix

--- wifi-monitor.sh
+++ wifi-monitor.sh
@@ -3,7 +3,7 @@ ping -c2 www.google.com > /dev/null
 
 if [ $? != 0 ]
 then
-    sleep 60
+    sleep 30
 
     sudo ifdown --force eth0
     sudo ifdown --force wlan0
@@ -11,8 +11,6 @@ then
     sudo /etc/init.d/networking restart
     sudo systemctl restart avahi-daemon.service
     sudo systemctl restart wpa_supplicant.service
-
-    sleep 3
 
     echo $(date; iwgetid -r) >> reconnect.log
     #/sbin/ifconfig >> reconnect.log
