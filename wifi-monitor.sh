a16848dcb951ee776a64f85bddc07bfb17b7fb55
Tomokatsu Kumata <tkumata@gmail.com>
2018-08-09 11:58:25 +0900

    test.

--- wifi-monitor.sh
+++ wifi-monitor.sh
@@ -8,6 +8,7 @@ ping -c2 www.google.com > /dev/null
 # non-zero is false.
 if [ $? != 0 ]
 then
+    # Wait for AP finishes boot.
     sleep 40
 
     # Turn off power management for RPi3 WiFi.
@@ -17,23 +18,29 @@ then
     sudo ifdown --force eth0
     sudo ifdown --force wlan0
     sudo systemctl daemon-reload
-    sudo /etc/init.d/networking restart
+    #sudo /etc/init.d/networking restart
+    sudo systemctl stop dhcpcd.service
+    sudo systemctl restart networking.service
     sudo systemctl restart avahi-daemon.service
     sudo systemctl restart wpa_supplicant.service
+    sudo systemctl start dhcpcd.service
+    sudo ifup --force wlan0
     wpa_cli -i wlan0 reconfigure
 
     # Log
-    echo $(date; iwgetid -r) >> reconnect.log
-    #ifconfig wlan0 >> reconnect.log
-    #iwconfig wlan0 >> reconnect.log
+    #echo $(date; iwgetid -r) >> reconnect.log
+    ifconfig >> reconnect.log
+    #iwconfig >> reconnect.log
     #systemctl status >> recoonect.log
+
+    sleep 5
 
     # So give up.
     if ifconfig wlan0 | grep '192.168.2.1' > /dev/null
     then
-        echo "OK." >> reconnect.log
+        echo $(date; iwgetid -r; echo "OK.") >> reconnect.log
     else
-        echo "NG. Need reboot." >> reconnect.log
+        echo $(date; iwgetid -r; echo "NG. Need reboot.") >> reconnect.log
         sudo reboot
     fi
 fi
